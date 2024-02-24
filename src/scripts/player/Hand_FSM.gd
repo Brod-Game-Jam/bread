2dc1310a6aeb783797eb0754bd50130100231deb
Julia <julia.v.wang@gmail.com>
2024-02-24 01:10:13 +0100

    set mouse cursor

--- src/scripts/player/Hand_FSM.gd
+++ src/scripts/player/Hand_FSM.gd
@@ -1,23 +1,29 @@
 extends FiniteStateMachine
 class_name Hand_FSM
 
+# Load the custom images for the mouse cursor.
+var bread = load("res://visual/legacy/bread_32.png")
+var bite = load("res://visual/legacy/bite_bread_32.png")
+
 func _ready():
 	super() # Run function from FiniteStateMachine
-	
-	# Set cursor to invisble when in game (move to FSM possibly)
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_custom_mouse_cursor(bite)
+	# Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _change_state(new_state: State):
 	super(new_state)
 	
-	# TODO: Set cursor to visible when biting
+	# TODO: Set cursor to visible when biting and visible when active
 	if current_state.get_name() == "Grabbed":
+		Input.set_custom_mouse_cursor(bite)
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if current_state.get_name() == "Active":
+	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		Input.set_custom_mouse_cursor(bread)
 	
 func _process(delta):
 	super(delta)
 	
 func _physics_process(delta):
 	super(delta)
+
