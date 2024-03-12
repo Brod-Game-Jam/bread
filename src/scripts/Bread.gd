a007a8b4390ad1e940b2a2d93fe6c12c1cad8eb0
Sickan <jessicagorwat@gmail.com>
2024-03-12 11:49:19 +0100

    Set reset_state false in _toaster_expulsion instead of _integrate_forces to solve bread freeze bug. Remove redundant code

--- src/scripts/Bread.gd
+++ src/scripts/Bread.gd
@@ -11,28 +11,16 @@ signal bread_dropped
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# COMMENT: Dessa functioner verkar kallas direkt från state machine nuförtiden så de behöver inte vara kopplade till signaler
-	#var state_toasting = get_node("../../FiniteStateMachine/Toasting")
-	#var state_airborne = get_node("../../FiniteStateMachine/Airborne")
-	# state_toasting.connect("state_entered",_reset_bread)
-	# state_airborne.connect("state_entered",_toaster_expulsion)
-	
 	# Retrieve nodes
 	toaster = get_node("../Toaster")
 	
-	# Place bread in toaster
-	transform = Transform2D(0.0,toaster.position)
-	freeze = true
-
 func _reset_bread():
 	reset_state = true
 	
 # Launch bread out of toaster
 func _toaster_expulsion():
-	reset_state = false # WHY WHO IS FREEZING MY BREAD???
-	freeze = false
+	reset_state = false 
 	apply_impulse(Vector2(0,-1000))
-	pass
 	
 # Is run every physics update (?)
 func _integrate_forces(state):
@@ -41,13 +29,10 @@ func _integrate_forces(state):
 		state.linear_velocity = Vector2.ZERO
 		state.angular_velocity = 0
 		state.transform = Transform2D(0.0,toaster.position)
-		freeze = true
-		reset_state = false
 		
 	# Checks for dropped bread
 	if position.y > get_viewport_rect().size.y:
 		bread_dropped.emit()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
