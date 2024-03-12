6321108343e69ea149b663db754ee4585fea90df
Sickan <jessicagorwat@gmail.com>
2024-03-12 19:46:33 +0100

    Add code skeleton for Hurt

---
+++ src/scripts/player/Hurt.gd
@@ -0,0 +1,15 @@
+extends  State
+
+func _enter_state():
+	# TODO start cooldown countdown
+	pass
+
+func _exit_state():
+	pass
+
+func _state_update(_delta: float):
+	# TODO countdown and state change
+	pass
+
+func _state_physics_update(_delta: float):
+	pass
