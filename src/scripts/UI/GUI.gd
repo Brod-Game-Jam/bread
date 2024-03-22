4865e4426643d7149af10261033d82a07ed6c17b
Julia <julia.v.wang@gmail.com>
2024-03-22 02:12:52 +0100

    Fix hearts and organise

--- src/scripts/UI/GUI.gd
+++ src/scripts/UI/GUI.gd
@@ -1,13 +1,21 @@
 extends CanvasLayer
 
+@export var heart_file:Texture2D
 var fsm
+var child
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fsm = get_node("../../FiniteStateMachine")
+	
 	$Heat.max_value = fsm.max_temperature
 	$AnimationPlayer.play("Bite")
+	update_health(fsm.current_lives) # Connect with FSM for updates
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Heat.value = fsm.temperature
+
+func update_health(value):
+	for i in fsm.max_lives:
+		$Health.get_child(i).visible = value > i
--- src/scripts/UI/MainMenu.gd
+++ src/scripts/UI/MainMenu.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+extends CanvasLayer
 
 @onready var labels = $Main
 @onready var settings = $Settings
